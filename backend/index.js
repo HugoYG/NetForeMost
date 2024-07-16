13f795330ee56ccf06d92120643541f9907bdadf
HugoYG <hugoyounggolden@gmail.com>
2024-07-16 05:06:12 -0600

    adding first version of the task, i need to reorganize

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,49 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
 mongoose.connect('mongodb://localhost/netforemost', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
+    .then(async () => {
         console.log('Connected to MongoDB');
+        try {
+            const documentNumbers = await Article.countDocuments();
+            console.log('Document numbers:', documentNumbers);
+            if (documentNumbers === 0) {
+                const data = JSON.parse(fs.readFileSync('../data.json'));
+                await Article.insertMany(data);
+                console.log('Data inserted successfully!');
+            }
+            else {
+                console.log('Collection is not empty, I can skip the data insertion!');
+            }
+        } catch (error) {
+            console.error('Error with handling the data:', err);
+        }
+
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
     });
 
-// Load data from data.json
-const data = JSON.parse(fs.readFileSync('data.json'));
+const articleSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    description: String,
+    url: String,
+    urlToImage: String,
+    publishedAt: Date
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+app.get('/articles', async (req, res) => {
+    try {
+        const articles = await Article.find({});
+        res.json(articles);
+    } catch (error) {
+        console.error('Error fetching articles:', error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
