d374c4293426ae2c4c66baf35b660356b84ba01d
HugoYG <hugoyounggolden@gmail.com>
2024-07-16 05:24:55 -0600

    reorganizing things a little bit, extracing db connection, data, and model

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const fs = require('fs');
+
+
+const Article = require('./models/article.model');
+const connectDB = require('./config/db');
 
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost/netforemost', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(async () => {
-        console.log('Connected to MongoDB');
-        try {
-            const documentNumbers = await Article.countDocuments();
-            console.log('Document numbers:', documentNumbers);
-            if (documentNumbers === 0) {
-                const data = JSON.parse(fs.readFileSync('../data.json'));
-                await Article.insertMany(data);
-                console.log('Data inserted successfully!');
-            }
-            else {
-                console.log('Collection is not empty, I can skip the data insertion!');
-            }
-        } catch (error) {
-            console.error('Error with handling the data:', err);
-        }
+connectDB();
 
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
-
-const articleSchema = new mongoose.Schema({
-    author: String,
-    title: String,
-    description: String,
-    url: String,
-    urlToImage: String,
-    publishedAt: Date
-});
-
-const Article = mongoose.model('Article', articleSchema);
 
 app.get('/articles', async (req, res) => {
     try {
